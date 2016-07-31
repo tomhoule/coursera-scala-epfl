4365c761f2d38a50758625e3764934b5295e67cb
Tom Houlé <tom@kafunsho.be>
2016-08-01 00:00:19 +0200

    Finalize the last assignment

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -11,11 +11,19 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions.mapValues((expression: Signal[Expr]) =>
+      Signal { eval(expression(), namedExpressions) })
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(v) => v
+      case Ref(v) => eval(getReferenceExpr(v, references), references.filterKeys((key: String) => key != v))
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+    }
   }
 
   /** Get the Expr for a referenced variables.

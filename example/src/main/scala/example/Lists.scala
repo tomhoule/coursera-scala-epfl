d88beadf6c8b53da81be5987220782437e7b8428
Tom Houlé <tom@tomhoule.com>
2017-03-11 23:54:34 +0100

    Get better score on first week of spark course

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -23,7 +23,7 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = xs.reduce((elem, acc) => elem + acc)
+    def sum(xs: List[Int]): Int = xs.foldLeft(0)((elem, acc) => elem + acc)
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +38,5 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = xs.reduce((acc, elem) => Math.max(acc, elem))
+    def max(xs: List[Int]): Int = if (xs.isEmpty) throw new java.util.NoSuchElementException else xs.reduce((acc, elem) => Math.max(acc, elem))
   }

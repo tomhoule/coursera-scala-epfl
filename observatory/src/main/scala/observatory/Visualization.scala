c757f495df36a87200b62703d227c2c011fc1c45
Tom Houlé <tom@tomhoule.com>
2017-05-26 18:47:52 +0200

    Complete week 2

--- observatory/src/main/scala/observatory/Visualization.scala
+++ observatory/src/main/scala/observatory/Visualization.scala
@@ -7,13 +7,58 @@ import com.sksamuel.scrimage.{Image, Pixel}
   */
 object Visualization {
 
+  val p: Double = 2
+
+  val radiusOfEarth = 6371 // km
+
+  def absoluteDifference(a: Double, b: Double): Double = {
+    val absA = Math.abs(a)
+    val absB = Math.abs(b)
+    if (absA > absB)
+      absA - absB
+    else
+      absB - absA
+  }
+
+  def gpsToRadians(value: Double): Double = {
+    (Math.PI/180) * value
+  }
+
+  // Great-circle distance in km
+  // https://en.wikipedia.org/wiki/Great-circle_distance
+  def distance(loc1: Location, loc2: Location): Double = {
+    val a = Math.sin(gpsToRadians(loc1.lat)) * Math.sin(gpsToRadians(loc2.lat))
+    val b = Math.cos(gpsToRadians(loc1.lat)) * Math.cos(gpsToRadians(loc2.lat)) * Math.cos(
+      gpsToRadians(loc2.lon - loc1.lon))
+    val centralAngle = Math.acos(a + b)
+
+    centralAngle * radiusOfEarth
+  }
+
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
     * @param location Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+
+    def w(dist: Double): Double = {
+      1 / Math.pow(dist, p)
+    }
+
+    var top: Double = 0
+    var bottom: Double = 0
+
+    for (temp <- temperatures) {
+      if (location == temp._1) return temp._2
+      val dist = distance(temp._1, location)
+      if (dist <= 1) return dist
+      val wdist = w(dist)
+      top += wdist * temp._2
+      bottom += wdist
+    }
+
+    top / bottom
   }
 
   /**
@@ -22,7 +67,41 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+    val sortedPoints = points.toIndexedSeq.sortBy(_._1)
+    var previous = points.minBy(_._1)
+    var next = points.maxBy(_._1)
+
+    for (point <- sortedPoints) {
+      if (point._1 == value)
+        return point._2
+      if (point._1 <= value && point._1 > previous._1)
+        previous = point
+      if (point._1 >= value && point._1 < next._1)
+        next = point
+    }
+
+    if (next._1 < value)
+      return next._2
+
+    if (previous._1 > value)
+      return previous._2
+
+    val red = ((previous._2.red * (next._1 - value)) + (next._2.red * (value - previous._1))) / (next._1 - previous._1)
+    val green = ((previous._2.green * (next._1 - value)) + (next._2.green * (value - previous._1))) / (next._1 - previous._1)
+    val blue = ((previous._2.blue * (next._1 - value)) + (next._2.blue * (value - previous._1))) / (next._1 - previous._1)
+
+    Color(
+      Math.min(255, Math.round(red.toFloat)),
+      Math.min(255, Math.round(green.toFloat)),
+      Math.min(255, Math.round(blue.toFloat))
+    )
+  }
+
+  def coordinates(pixelCoord: Int, w: Int, h: Int): Location = {
+    val row = Math.round(Math.floor(pixelCoord / w))
+    val col = pixelCoord % w
+
+    Location(90 - row, col - 180)
   }
 
   /**
@@ -31,7 +110,21 @@ object Visualization {
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
-    ???
+    def pixel(color: Color): Pixel = {
+      Pixel(color.red, color.green, color.blue, 1)
+    }
+
+    val w = 360
+    val h = 180
+    val pixelsNumber = w * h
+
+    val pixels: Array[Pixel] = new Array(pixelsNumber)
+
+    for (i <- 0 until pixelsNumber) {
+      pixels(i) = pixel(interpolateColor(colors, predictTemperature(temperatures, coordinates(i, w, h))))
+    }
+
+    Image(w, h, pixels)
   }
 
 }
--- observatory/src/test/scala/observatory/VisualizationTest.scala
+++ observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -9,5 +9,88 @@ import org.scalatest.prop.Checkers
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
 
+  test("distance between Gap and Marseille is right") {
+    val gap = Location(44.4465, 6.5176)
+    val marseille = Location(43.1821, 5.809)
+    assert(Visualization.distance(gap, marseille) > 150)
+    assert(Visualization.distance(gap, marseille) < 155)
+  }
 
+  test("distance between Berlin and Paris is approximately right") {
+    val berlin = Location(52.51469, 13.43291)
+    val paris = Location(48.870991, 2.384650)
+    assert(Visualization.distance(berlin, paris) > 870)
+    assert(Visualization.distance(berlin, paris) < 880)
+  }
+
+  test("distance between Murmansk and London is approximately right") {
+    val london = Location(51.51200, -0.121900)
+    val murmansk = Location(68.9712, 33.055)
+    assert(Visualization.distance(london, murmansk) > 2600)
+    assert(Visualization.distance(murmansk, london) < 2700)
+  }
+
+  test("predictTemperature does not crash with small input") {
+    val input = Set().toIterable
+    Visualization.predictTemperature(input, Location(1, -1))
+  }
+
+  test("predictTemperature gives a reasonable answer") {
+    val input = Set(
+      (Location(51.51200, -0.121900), 20.0),
+      (Location(51.51200, -0.121900), 23.0),
+      (Location(51.51200, -0.121900), 18.0),
+      (Location(68.9712, 33.055), -44.9)
+    ).toIterable
+    assert(Visualization.predictTemperature(input, Location(51.0, 2.1)) > 20)
+    assert(Visualization.predictTemperature(input, Location(51.0, 2.1)) < 21)
+  }
+
+  test("interpolateColor predicts the correct color") {
+    val points = Set(
+      (-55.0, Color(255, 0, 255)),
+      (55.0, Color(0, 255, 0))
+    ).toIterable
+    assert(Visualization.interpolateColor(points, 0) === Color(128, 128, 128))
+  }
+
+  test("interpolateColor predicts the correct color (more complex)") {
+    val points = Set(
+      (-55.0, Color(255, 0, 255)),
+      (-10.0, Color(200, 200, 200)),
+      (55.0, Color(0, 255, 0))
+    ).toIterable
+    assert(Visualization.interpolateColor(points, 12) === Color(132, 219, 132))
+  }
+
+  test("coordinates seems to work") {
+    val topLeft = 0
+    val end = (360 * 180)
+    val origin = (360 * 90) + 180
+
+    assert(Visualization.coordinates(topLeft, 360, 180) === Location(90, -180))
+    assert(Visualization.coordinates(origin, 360, 180) === Location(0, 0))
+    assert(Visualization.coordinates(end, 360, 180) === Location(-90, -180))
+  }
+
+  test("interpolateColor handles extreme values correctly") {
+    assert(Visualization.interpolateColor(
+      List((-1.0, Color(255,0,0)), (1.0, Color(0, 0, 255))),
+      -1.0
+    ) === Color(255, 0, 0))
+
+    assert(Visualization.interpolateColor(
+      List((-100.0,Color(255,0,0)), (-19.793294989955413,Color(0,0,255))),
+      -110
+    ) === Color(255, 0, 0))
+  }
+
+  test("predictTemperature does not mix up distances") {
+    val input = Set(
+      (Location(51.51200, -0.121900), 20.0),
+      (Location(31.51200, 22.12), 10.0)
+    ).toIterable
+    assert(Visualization.predictTemperature(input, Location(51.5, 5.2)) > 19)
+    assert(Visualization.predictTemperature(input, Location(51.5, 5.2)) < 20)
+  }
 }
